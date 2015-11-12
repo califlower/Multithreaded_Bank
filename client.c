35b3e5dc9a656f10a4c7c355dc849e47f18bd870
Cal <cal13199@gmail.com>
2015-11-11 20:38:24 -0500

    Update client.c

--- client.c
+++ client.c
@@ -13,35 +13,18 @@ int main(int argc, char ** argv)
 	struct hostent * host;
 	int len;
 	
-
 	/* create socket */
 	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (sock <= 0)
-	{
-		fprintf(stderr, "%s: error: cannot create socket\n", argv[0]);
-		return -3;
-	}
 
 	/* connect to server */
 	address.sin_family = AF_INET;
 	address.sin_port = htons(port);
 	host = gethostbyname(argv[1]);
-	if (!host)
-	{
-		fprintf(stderr, "%s: error: unknown host %s\n", argv[0], argv[1]);
-		return -4;
-	}
+
 	memcpy(&address.sin_addr, host->h_addr_list[0], host->h_length);
 	
-	if (connect(sock, (struct sockaddr *)&address, sizeof(address)))
-	{
-		fprintf(stderr, "%s: error: cannot connect to host %s\n", argv[0], argv[1]);
-		return -5;
-	}
+	connect(sock, (struct sockaddr *)&address, sizeof(address));
 
-	
-	
-	
 	printf("Connection to Online Bank Successful\n");
 	printf("Welcome to Your Online Bank\n");
 	printf("-----------------------------\n");
