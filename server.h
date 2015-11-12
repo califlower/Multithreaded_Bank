a32511fd549ee18b8d43972c50fd615c39a446cf
saif429 <saif429@gmail.com>
2015-11-12 11:20:38 -0500

    Update server.h

--- server.h
+++ server.h
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
 
+typedef struct
+{
+	int sock;
+	struct sockaddr address;
+	socklen_t addr_len;
+
+} connection_t;
+
+
+typedef struct
+{
+	char accountName[100];
+	float balance;
+	int inUse;
+} account;
