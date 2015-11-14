68593403d86554421ababf36494129d9e3affa06
Cal <cal13199@gmail.com>
2015-11-14 15:00:47 -0500

    Update server.h

--- server.h
+++ server.h
@@ -1,15 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <string.h>
+#include			<stdio.h>
+#include 			<stdlib.h>
+#include 			<pthread.h>
+#include 			<sys/socket.h>
+#include 			<netinet/in.h>
+#include 			<unistd.h>
+#include 			<string.h>
 
-#define pNum 2500
-#define maxAcc 20
-#define strSize 1000
-#define maxName 100
+/*****************************
+ * pNum is the port number of the server
+ * maxAcc is the maximum number of accounts the server can hold
+ * strSize is a magic number that I've been setting the strings too. Feel free to make it bigger
+ 	Any smaller than like, 30 though and stuff stops working. Keep it above 100 almost always
+ 	Keep it at a 1000 though because it works
+ *maxName is the maximum size of an account name
+******************************/
+
+
+#define pNum 			2500
+#define maxAcc 			20
+#define strSize 		1000
+#define maxName 		100
 
 typedef struct
 {
